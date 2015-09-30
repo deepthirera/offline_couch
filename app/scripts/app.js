ab100bbbf3bd0d5e854c9be461404fcf109bb052
Deepthi <deepthirera@gmail.com>
2015-09-30 14:31:46 +0530

    save and retrieve in pouch

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,26 +3,53 @@ var React = window.React = require('react'),
     Timer = require("./ui/Timer"),
     mountNode = document.getElementById("app");
 
+var db = new PouchDB('sources');
+var remoteCouch = "http://localhost:5984/sources";
+
 var TodoList = React.createClass({
   render: function() {
     var createItem = function(itemText) {
-      return <li>{itemText}</li>;
+        return <li>{itemText.title}</li>;
     };
     return <ul>{this.props.items.map(createItem)}</ul>;
   }
 });
+
 var TodoApp = React.createClass({
   getInitialState: function() {
-    return {items: [], text: ''};
+      return {items: [], text: ''};
   },
+
+    componentDidMount: function() {
+        var srcs = [];
+        var self=this;
+        db.allDocs({include_docs: true, descending: true}, function(err, response) {
+                srcs = response.rows.map(function(src){return src.doc})
+                if (self.isMounted()) {
+                    self.setState({
+                        items: srcs,
+                        text: ''
+                    });
+                }
+            }
+        );
+    },
   onChange: function(e) {
     this.setState({text: e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    var nextItems = this.state.items.concat([this.state.text]);
-    var nextText = '';
-    this.setState({items: nextItems, text: nextText});
+      var self=this;
+      var src = {
+          _id: new Date().toISOString(),
+          title: self.state.text,
+          url: "www.url.com"
+      };
+      db.put(src, function callback(err, result) {
+        var nextItems = self.state.items.concat([src]);
+        var nextText = '';
+        self.setState({items: nextItems, text: nextText});
+      });
   },
   render: function() {
     return (
@@ -33,7 +60,6 @@ var TodoApp = React.createClass({
           <input onChange={this.onChange} value={this.state.text} />
           <button>{'Add #' + (this.state.items.length + 1)}</button>
         </form>
-        <Timer />
       </div>
     );
   }
